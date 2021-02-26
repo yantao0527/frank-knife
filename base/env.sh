2f246bf714aadf76651dccfda94db065ae13f06e
yantao0527 <yantao0527@gmail.com>
2021-02-26 22:30:48 +0800

    恢复

--- base/env.sh
+++ base/env.sh
@@ -1,7 +1,3 @@
 
 export ALICLOUD_ACCESS_KEY="anaccesskey"
 export ALICLOUD_SECRET_KEY="asecretkey"
-
-# yantao0527
-export ALICLOUD_ACCESS_KEY="w55LJEnfOG6inBYv"
-export ALICLOUD_SECRET_KEY="eYKr6F62sKt69RCPxvtr10NROsjFmM"
